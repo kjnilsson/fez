d866567001ff75b496a8ab0906b06a7f00481e60
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-06-17 13:19:27 +0200

    Set exit code

--- src/fez/Program.fs
+++ src/fez/Program.fs
@@ -14,22 +14,28 @@ let writeCoreFile dir name text =
 
 [<EntryPoint>]
 let main argv =
-    let sysCoreLib = typeof<System.Object>.GetTypeInfo().Assembly.Location
-    let sysPath = Path.GetDirectoryName(sysCoreLib)
-    let files = argv |> Array.map (|FullPath|)
-    let checker = FSharpChecker.Create(keepAssemblyContents = true)
-    let options = projectOptions checker files
-    for file in files do
-        let fileContents = File.ReadAllText file
-        let dir = Path.GetDirectoryName file
-        let res = check checker options file fileContents
-        let decs = res.AssemblyContents.ImplementationFiles.Head.Declarations
-        for implFile in res.AssemblyContents.ImplementationFiles do
-          for decl in implFile.Declarations do
-              (* failwithf "%A" decl *)
-              let modules = processDecl decl
-              for n, m in modules do
-                  (* printfn "final ast: %A" m *)
-                  cerl.prt m |> writeCoreFile dir n
-                  printfn "OUTFILE: %s/%s" dir n
-    0
+    try
+        let sysCoreLib = typeof<System.Object>.GetTypeInfo().Assembly.Location
+        let sysPath = Path.GetDirectoryName(sysCoreLib)
+        let files = argv |> Array.map (|FullPath|)
+        let checker = FSharpChecker.Create(keepAssemblyContents = true)
+        let options = projectOptions checker files
+        for file in files do
+            let fileContents = File.ReadAllText file
+            let dir = Path.GetDirectoryName file
+            let res = check checker options file fileContents
+            let decs = res.AssemblyContents.ImplementationFiles.Head.Declarations
+            for implFile in res.AssemblyContents.ImplementationFiles do
+              for decl in implFile.Declarations do
+                  (* failwithf "%A" decl *)
+                  let modules = processDecl decl
+                  for n, m in modules do
+                      (* printfn "final ast: %A" m *)
+                      cerl.prt m |> writeCoreFile dir n
+                      printfn "OUTFILE: %s/%s" dir n
+        0
+    with
+    | e ->
+        Environment.Exit 1
+        reraise()
+        1
