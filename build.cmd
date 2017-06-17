fe8f6bba7bcc48c1fc2558c7bc7c2b71f07dfea4
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-06-17 11:48:32 +0200

    erlc all the things

--- build.cmd
+++ build.cmd
@@ -2,4 +2,4 @@ mkdir ebin
 dotnet restore src/fez/fez.fsproj
 dotnet restore ./test/test.fsproj
 dotnet build src/fez/fez.fsproj
-erlc +clint -o ./ebin ./src/*.erl
+FOR %%f in (src\*.erl) DO erlc +clint -o ebin "%%f"
