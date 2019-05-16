c1d7c122d338a0b0c2eb59d7f584b2a73630179e
kjnilsson <knilsson@pivotal.io>
2019-05-16 10:43:29 +0200

    wip

--- src/fez.core/core.fs
+++ src/fez.core/core.fs
@@ -4,6 +4,8 @@ module Fez.Core
 type Pid = P
 type Port = Port
 type Node = Node
+type Reference = Reference
+type Atom = Atom of string
 
 type Term =
     | Atom of string
@@ -21,6 +23,7 @@ type Dst =
     | Port of Port
     | RegName of string
 
+let reference() = Reference
 
 let receive<'Msg> () =
     // dummy, this is never called
@@ -45,11 +48,15 @@ type ModCall(modu: string, func: string) =
 type CaseStrategy =
     | LowerFirstCharLeaveAllUpper = 0
 
+[<System.AttributeUsage(System.AttributeTargets.Class)>]
 type ErlangTerm() =
     inherit System.Attribute()
     member val IncludeTagsWithTuples = false with get, set
     member val CaseStrategy = CaseStrategy.LowerFirstCharLeaveAllUpper with get, set
 
+[<System.AttributeUsage(System.AttributeTargets.Interface)>]
+type ErlangBehaviour() =
+    inherit System.Attribute()
 
 type Ref<'a> with
     member r.release () : 'a option =
@@ -58,6 +65,65 @@ type Ref<'a> with
 type System.Lazy<'a> with
     member l.release () : 'a option =
         Some l.Value
+
+[<ErlangTerm>]
+type GenServerTimeout =
+    | Timeout of int
+    | Infinity
+
+[<ErlangTerm>]
+type From =
+    | From of Pid * Reference
+
+[<ErlangTerm>]
+type CastResult<'TState> =
+    | Noreply of 'TState
+    | Stop  of Term * 'TState
+
+[<ErlangTerm>]
+type CallResult<'TState, 'TReply> =
+    | Noreply of 'TState
+    | Reply of 'TReply * 'TState
+    | Stop  of Term * 'TState
+
+[<ErlangBehaviour>]
+type IGenServer<'TArgs, 'TState, 'TRequest>  =
+    abstract member init : 'TArgs  -> 'TState
+    abstract member handle_cast : 'TRequest -> 'TState ->
+        CastResult<'TState>
+    abstract member handle_call : 'TRequest -> From -> 'TState ->
+        CallResult<'TState, 'TRequest>
+    abstract member handle_info : Term -> 'TState ->
+        CastResult<'TState>
+
+module GenServer =
+    [<ErlangTerm(IncludeTagsWithTuples = true)>]
+    type ServerName =
+        | Local of Atom
+        | Global of Term
+        | Via of Atom * Term
+
+    [<ErlangTerm(IncludeTagsWithTuples = true)>]
+    type StartLinkResult =
+        | Ok of Pid
+        | Error of Term
+
+    let start_link<'TArgs> (name : ServerName) (modu : Atom) (args : 'TArgs) =
+        Error
+
+type MyGenServer private () =
+    interface IGenServer<int, int, int> with
+        member __.init args =
+            args
+        member __.handle_cast req state =
+            CastResult.Noreply state
+        member __.handle_call req from state =
+            CallResult.Reply (55, state)
+        member __.handle_info info state =
+            CastResult.Noreply state
+    static member start_link () = ""
+
+
 
 /// binaries are represented as byte arrays and manipulated through the
 /// Binary module
@@ -70,3 +136,4 @@ module Binary =
 
     let part (pos :int) (len: int) (subject : Binary) =
         [||]
+
